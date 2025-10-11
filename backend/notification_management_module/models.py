be86afe5f8245032642d5b0a598c14dcf1f2366a
Justine Toong <programmerjustine@gmail.com>
2025-10-11 20:22:35 +0800

    Added: Models for notification management module

--- backend/notification_management_module/models.py
+++ backend/notification_management_module/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Notification(models.Model):
+    STATUS_CHOICE = [
+        ('Unsent', 'Unsent'),
+        ('Sent', 'Sent')
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
+    message = models.TextField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICE, default="Unsent")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
