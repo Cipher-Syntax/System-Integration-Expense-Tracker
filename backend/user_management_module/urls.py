af45df4d641bfb954e27a54a57edc9f4c38584ed
Justine Toong <programmerjustine@gmail.com>
2025-10-12 07:36:43 +0800

    Updated: Using cookies to get access and reffresh token

--- backend/user_management_module/urls.py
+++ backend/user_management_module/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #type: ignore
-from .views import CreateUserView, UpdateUserView, PasswordResetRequestView, PasswordResetConfirmView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView #type: ignore
+from .views import CreateUserView, UpdateUserView, PasswordResetRequestView, PasswordResetConfirmView, CookieTokenObtainPairView, CookieTokenRefreshView
 
 
 urlpatterns = [
     path('register/', CreateUserView.as_view(), name='user-register'),
     path('profile/', UpdateUserView.as_view(), name='user-update'),
-    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    # path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/', CookieTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', CookieTokenRefreshView.as_view(), name='token-refresh'),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('reset-password/<uid>/<token>/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
