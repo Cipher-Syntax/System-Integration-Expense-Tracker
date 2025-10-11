ef39c587fa1eb1a17c47631983bef965965194c3
Justine Toong <programmerjustine@gmail.com>
2025-10-11 20:21:23 +0800

    Added: Models for budget management module

--- backend/budget_management_module/models.py
+++ backend/budget_management_module/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Budget(models.ForeignKey):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="budgets")
+    limit_amount = models.DecimalField(max_digits=10000, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
