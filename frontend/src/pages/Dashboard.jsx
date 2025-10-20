66c5b9c857c1e97f72b8d2154b36f877faff3263
Justine Toong <programmerjustine@gmail.com>
2025-10-20 23:14:24 +0800

    Added: Dashboard layout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <section className='mt-26'>
+            <h1 className='font-bold leading-relaxed tracking-widest text-2xl'>DASHBOARD</h1>
+
+            <div className='w-[400px] h-[200px] bg-white rounded-2xl p-5'>
+                <h2 className='text-gray-500'>Available Balance</h2>
+                <h3 className='text-3xl'>₱ 10,000</h3>
+            </div>
+        </section>
     )
 }
 
