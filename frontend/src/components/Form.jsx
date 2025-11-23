46c118d31addafbbf8365809ba91ec15386dcaaa
Justine Toong <programmerjustine@gmail.com>
2025-11-23 10:39:15 +0800

    Deployment attempt fix 3

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Wallet, TrendingUp, PieChart, ArrowRight, Eye, EyeOff } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
